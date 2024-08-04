b0f983ca458b6f3444cadc9b13bdaf1330ca47e9
Brandi Seeley <brandi.n.seeley@gmail.com>
2024-08-04 16:47:47 +0900

    Create test for check constraint ensuring start is before stop

--- scheduler/tests.py
+++ scheduler/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from datetime import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.db.utils import IntegrityError
+
+from .models import TimeRange
+
+class TimeRangeTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_creating_reversed_range(self):
+        """Creating a range with end before start raises an error"""
+        earlier = datetime(2024, 8, 4)
+        later = datetime(2024, 10, 4)
+        time_range = TimeRange(start_time=later, end_time=later)
+        self.assertRaises(IntegrityError, time_range.save)
