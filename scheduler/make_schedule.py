0bc54dc9eb7007f06cce7fa0f8c4f8bb9c4fff39
Brandi Seeley <brandi.n.seeley@gmail.com>
2024-08-11 17:25:57 +0900

    Make datetime objects time zone aware

--- scheduler/make_schedule.py
+++ scheduler/make_schedule.py
@@ -1,13 +1,17 @@
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 from scheduler.models import TimeRange, TimeRangeUnion, Schedule
+
+# Define the timezone
+timezone = ZoneInfo('Asia/Seoul')
 
 # Create the main schedule
 schedule = Schedule.objects.create(title="Meeting Next Week")
 schedule.save()
 
-monday = datetime(2024, 9, 2, 7)
-friday = datetime(2024, 9, 6, 17)
+monday = datetime(2024, 9, 2, 7, tzinfo=timezone)
+friday = datetime(2024, 9, 6, 17, tzinfo=timezone)
 main_range = TimeRange.objects.create(start_time=monday, end_time=friday)
 main_range.save()
 
@@ -18,13 +22,13 @@ schedule.add_main_union(main_union)
 # Create a user union
 
 # Create ranges
-tuesday_morning = datetime(2024, 9, 3, 8)
-tuesday_noon =    datetime(2024, 9, 3, 12)
+tuesday_morning = datetime(2024, 9, 3, 8, tzinfo=timezone)
+tuesday_noon =    datetime(2024, 9, 3, 12, tzinfo=timezone)
 tuesday_range = TimeRange.objects.create(start_time=tuesday_morning, end_time=tuesday_noon)
 tuesday_range.save()
 
-thursday_noon =    datetime(2024, 9, 5, 12)
-thursday_evening = datetime(2024, 9, 5, 17)
+thursday_noon =    datetime(2024, 9, 5, 12, tzinfo=timezone)
+thursday_evening = datetime(2024, 9, 5, 17, tzinfo=timezone)
 thursday_range = TimeRange.objects.create(start_time=thursday_noon, end_time=thursday_evening)
 thursday_range.save()
 
@@ -36,10 +40,10 @@ user1_union.add_range(thursday_range)
 # Add user union to schedule
 schedule.add_user_union(user1_union)
 
-# time1 = datetime(2024, 8, 6, 7, 30)
-# time2 = datetime(2024, 8, 6, 8, 30)
-# time3 = datetime(2024, 8, 6, 9, 30)
-# time4 = datetime(2024, 8, 6, 10, 30)
+# time1 = datetime(2024, 8, 6, 7, 30, tzinfo=timezone)
+# time2 = datetime(2024, 8, 6, 8, 30, tzinfo=timezone)
+# time3 = datetime(2024, 8, 6, 9, 30, tzinfo=timezone)
+# time4 = datetime(2024, 8, 6, 10, 30, tzinfo=timezone)
 
 # range1 = TimeRange(start_time=time2, end_time=time3)
 # range1.save()
--- scheduler/tests.py
+++ scheduler/tests.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 from django.test import TestCase
 from django.db.utils import IntegrityError
@@ -12,8 +13,8 @@ class TimeRangeTestCase(TestCase):
 
     def test_creating_range(self):
         """Creating a range with start before end works"""
-        earlier = datetime(2024, 8, 4)
-        later = datetime(2024, 10, 4)
+        earlier = datetime(2024, 8, 4, tzinfo=ZoneInfo('Asia/Seoul'))
+        later = datetime(2024, 10, 4, tzinfo=ZoneInfo('Asia/Seoul'))
         time_range = TimeRange(start_time=earlier, end_time=later)
         time_range.save()
         self.assertEqual(earlier, time_range.start_time)
@@ -21,23 +22,23 @@ class TimeRangeTestCase(TestCase):
 
     def test_creating_reversed_range(self):
         """Creating a range with end before start raises an error"""
-        earlier = datetime(2024, 8, 4)
-        later = datetime(2024, 10, 4)
+        earlier = datetime(2024, 8, 4, tzinfo=ZoneInfo('Asia/Seoul'))
+        later = datetime(2024, 10, 4, tzinfo=ZoneInfo('Asia/Seoul'))
         time_range = TimeRange(start_time=later, end_time=earlier)
         self.assertRaises(IntegrityError, time_range.save)
 
     def test_creating_same_start_end_range(self):
         """Creating a range with the same start and end raises an error"""
-        time = datetime(2024, 8, 4)
+        time = datetime(2024, 8, 4, tzinfo=ZoneInfo('Asia/Seoul'))
         time_range = TimeRange(start_time=time, end_time=time)
         self.assertRaises(IntegrityError, time_range.save)
 
     def test_less_than(self):
         """A TimeRange with an earlier start is less than a TimeRange with a later start"""
-        time1 = datetime(2024, 8, 6, 7, 30)
-        time2 = datetime(2024, 8, 6, 8, 30)
-        time3 = datetime(2024, 8, 6, 9, 30)
-        time4 = datetime(2024, 8, 6, 10, 30)
+        time1 = datetime(2024, 8, 6, 7, 30, tzinfo=ZoneInfo('Asia/Seoul'))
+        time2 = datetime(2024, 8, 6, 8, 30, tzinfo=ZoneInfo('Asia/Seoul'))
+        time3 = datetime(2024, 8, 6, 9, 30, tzinfo=ZoneInfo('Asia/Seoul'))
+        time4 = datetime(2024, 8, 6, 10, 30, tzinfo=ZoneInfo('Asia/Seoul'))
 
         range1 = TimeRange(start_time=time1, end_time=time3)
         range2 = TimeRange(start_time=time2, end_time=time4)
@@ -45,29 +46,29 @@ class TimeRangeTestCase(TestCase):
 
     def test_equal(self):
         """Two TimeRanges are equal if they have the same start and stop"""
-        start1 = datetime(2024, 8, 6, 7, 30)
-        start2 = datetime(2024, 8, 6, 7, 30)
-        stop1 = datetime(2024, 8, 6, 8, 30)
-        stop2 = datetime(2024, 8, 6, 8, 30)
+        start1 = datetime(2024, 8, 6, 7, 30, tzinfo=ZoneInfo('Asia/Seoul'))
+        start2 = datetime(2024, 8, 6, 7, 30, tzinfo=ZoneInfo('Asia/Seoul'))
+        stop1 = datetime(2024, 8, 6, 8, 30, tzinfo=ZoneInfo('Asia/Seoul'))
+        stop2 = datetime(2024, 8, 6, 8, 30, tzinfo=ZoneInfo('Asia/Seoul'))
         range1 = TimeRange(start_time=start1, end_time=stop1)
         range2 = TimeRange(start_time=start2, end_time=stop2)
         self.assertEqual(range1, range2)
 
     def test_not_equal(self):
         """Two TimeRanges with the same starts but different stops are not equal"""
-        start = datetime(2024, 8, 6, 7, 30)
-        stop1 = datetime(2024, 8, 6, 8, 30)
-        stop2 = datetime(2024, 8, 6, 9, 30)
+        start = datetime(2024, 8, 6, 7, 30, tzinfo=ZoneInfo('Asia/Seoul'))
+        stop1 = datetime(2024, 8, 6, 8, 30, tzinfo=ZoneInfo('Asia/Seoul'))
+        stop2 = datetime(2024, 8, 6, 9, 30, tzinfo=ZoneInfo('Asia/Seoul'))
         range1 = TimeRange(start_time=start, end_time=stop1)
         range2 = TimeRange(start_time=start, end_time=stop2)
         self.assertNotEqual(range1, range2)
 
     def test_add_with_overlap(self):
         """Adding two overlapping time ranges gives a larger range"""
-        time1 = datetime(2024, 8, 6, 7, 30)
-        time2 = datetime(2024, 8, 6, 8, 30)
-        time3 = datetime(2024, 8, 6, 9, 30)
-        time4 = datetime(2024, 8, 6, 10, 30)
+        time1 = datetime(2024, 8, 6, 7, 30, tzinfo=ZoneInfo('Asia/Seoul'))
+        time2 = datetime(2024, 8, 6, 8, 30, tzinfo=ZoneInfo('Asia/Seoul'))
+        time3 = datetime(2024, 8, 6, 9, 30, tzinfo=ZoneInfo('Asia/Seoul'))
+        time4 = datetime(2024, 8, 6, 10, 30, tzinfo=ZoneInfo('Asia/Seoul'))
         range1 = TimeRange(start_time=time1, end_time=time3)
         range2 = TimeRange(start_time=time2, end_time=time4)
 
@@ -77,10 +78,10 @@ class TimeRangeTestCase(TestCase):
 
     def test_add_without_overlap(self):
         """Adding two non-overlapping time ranges gives a larger range"""
-        time1 = datetime(2024, 8, 6, 7, 30)
-        time2 = datetime(2024, 8, 6, 8, 30)
-        time3 = datetime(2024, 8, 6, 9, 30)
-        time4 = datetime(2024, 8, 6, 10, 30)
+        time1 = datetime(2024, 8, 6, 7, 30, tzinfo=ZoneInfo('Asia/Seoul'))
+        time2 = datetime(2024, 8, 6, 8, 30, tzinfo=ZoneInfo('Asia/Seoul'))
+        time3 = datetime(2024, 8, 6, 9, 30, tzinfo=ZoneInfo('Asia/Seoul'))
+        time4 = datetime(2024, 8, 6, 10, 30, tzinfo=ZoneInfo('Asia/Seoul'))
         range1 = TimeRange(start_time=time1, end_time=time2)
         range2 = TimeRange(start_time=time3, end_time=time4)
 
@@ -90,10 +91,10 @@ class TimeRangeTestCase(TestCase):
 
     def test_sorting_ranges_without_overlap(self):
         """Sorting a list of ranges should result in ascending start times"""
-        time1 = datetime(2024, 8, 6, 7, 30)
-        time2 = datetime(2024, 8, 6, 8, 30)
-        time3 = datetime(2024, 8, 6, 9, 30)
-        time4 = datetime(2024, 8, 6, 10, 30)
+        time1 = datetime(2024, 8, 6, 7, 30, tzinfo=ZoneInfo('Asia/Seoul'))
+        time2 = datetime(2024, 8, 6, 8, 30, tzinfo=ZoneInfo('Asia/Seoul'))
+        time3 = datetime(2024, 8, 6, 9, 30, tzinfo=ZoneInfo('Asia/Seoul'))
+        time4 = datetime(2024, 8, 6, 10, 30, tzinfo=ZoneInfo('Asia/Seoul'))
         range1 = TimeRange(start_time=time1, end_time=time2)
         range2 = TimeRange(start_time=time3, end_time=time4)
 
